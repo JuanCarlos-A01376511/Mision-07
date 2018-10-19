2af1c0a39d598ee6ab75e2449590a72a4ef197ae
JuanCarlos-A01376511 <42353013+JuanCarlos-A01376511@users.noreply.github.com>
2018-10-18 19:41:47 -0500

    Add files via upload

---
+++ Mision07.py
@@ -0,0 +1,60 @@
+# Autor: Juan Carlos Flores García, A01376511. Grupo 4
+
+# Descripción: Programa que muestra un menu con 3 opciones: Calcular residuo y cociente de divisiones, encontrar el
+# número mayor de una serie de números y salir del menu.
+
+# La siguiente función calcula el residuo y cociente de una división para después imprimirlo.
+def probarDivisiones(dividendo, divisor):
+    residuo = dividendo
+    cociente = 0
+    while (divisor <= dividendo):
+        residuo -= divisor
+        cociente += 1
+
+    print(dividendo, "/", divisor, "=", cociente, ", sobra", residuo)
+
+# La siguiente función encuentra el mayor de una serie de números ingresados por el usuario y luego lo imprime.
+def encontrarMayor():
+    serieNumerica = 0
+    numeroMayor = 0
+    while (serieNumerica != -1):
+        serieNumerica = int(input("Teclea un número [-1 para salir]: "))
+        if serieNumerica =< -1:
+
+
+
+
+
+# La siguiente función imprime las opciones del menu y permite ingresar el número de la opción a elegir.
+def leerOpcionMenu():
+    print("Misión 07. Ciclos while")
+    print("Autor: Juan Carlos Flores García")
+    print("Matrícula: A01376511")
+    print("1. Calcular divisiones")
+    print("2. Encontrar el mayor")
+    print("3. Salir")
+    opcion = int(input("Teclea tu opción: "))
+    return opcion
+
+# La función principal se encarga de mostrar el menu y llamar a todas las demás funciones.
+def main():
+    opcion = leerOpcionMenu()
+    while opcion != 3:
+        if opcion == 1:
+            dividendo = int(input("Dividendo: "))
+            divisor = int(input("Divisor: "))
+            probarDivisiones(dividendo, divisor)
+        elif opcion == 2:
+            print("Teclea una serie de números para encontrar el mayor.")
+            encontrarMayor()
+        elif opcion >= 0 or opcion < 3:
+            print("Error, teclea una opción del menu.")
+        opcion = leerOpcionMenu()
+    print("ERROR, teclea 1, 2 o 3")
+
+
+
+# Llamada a la función main.
+main()
+
+
