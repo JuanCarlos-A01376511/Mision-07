e8f49b84feffde7dcc7387c74e6733b00b97a77e
JuanCarlos-A01376511 <42353013+JuanCarlos-A01376511@users.noreply.github.com>
2018-10-19 00:51:38 -0500

    Update Mision07.py

--- Mision07.py
+++ Mision07.py
@@ -7,11 +7,12 @@
 def probarDivisiones(dividendo, divisor):
     residuo = dividendo
     cociente = 0
-    while (divisor <= dividendo):
+    while (divisor <= residuo):
         residuo -= divisor
         cociente += 1
 
     print(dividendo, "/", divisor, "=", cociente, ", sobra", residuo)
+
 
 # La siguiente función encuentra el mayor de una serie de números ingresados por el usuario y luego lo imprime.
 def encontrarMayor():
@@ -19,10 +20,14 @@ def encontrarMayor():
     numeroMayor = 0
     while (serieNumerica != -1):
         serieNumerica = int(input("Teclea un número [-1 para salir]: "))
-        if serieNumerica =< -1:
-
-
-
+        if serieNumerica < -1:
+            print("No hay valor mayor")
+        elif serieNumerica > numeroMayor:
+            numeroMayor = serieNumerica
+    if numeroMayor == 0:
+        print("No hay valor mayor")
+    else:
+        print("El mayor es", numeroMayor)
 
 
 # La siguiente función imprime las opciones del menu y permite ingresar el número de la opción a elegir.
@@ -36,6 +41,7 @@ def leerOpcionMenu():
     opcion = int(input("Teclea tu opción: "))
     return opcion
 
+
 # La función principal se encarga de mostrar el menu y llamar a todas las demás funciones.
 def main():
     opcion = leerOpcionMenu()
@@ -48,13 +54,11 @@ def main():
             print("Teclea una serie de números para encontrar el mayor.")
             encontrarMayor()
         elif opcion >= 0 or opcion < 3:
-            print("Error, teclea una opción del menu.")
+            print("ERROR, teclea 1, 2 o 3")
         opcion = leerOpcionMenu()
-    print("ERROR, teclea 1, 2 o 3")
-
+    print("Gracias por usar este programa, regresa pronto.")
 
 
 # Llamada a la función main.
 main()
-
 
